cf642bb80113dd9ed3599c53f28032f2cacf804e
Jerahmeel Cosinas <me@jerahmeelcosinas.net>
2019-12-25 22:04:35 +1100

    render a prefab

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,53 @@
 use amethyst::{
+    assets::{PrefabLoader,PrefabLoaderSystem,RonFormat},
     core::transform::TransformBundle,
-    ecs::prelude::{ReadExpect, Resources, SystemData},
     prelude::*,
     renderer::{
-        plugins::{RenderFlat2D, RenderToWindow},
+        plugins::{RenderShaded3D, RenderToWindow},
+        rendy::mesh::{Normal, Position, TexCoord},
         types::DefaultBackend,
         RenderingBundle,
     },
-    utils::application_root_dir,
+    utils::{application_root_dir,scene::BasicScenePrefab},
 };
 
-struct MyState;
+type MyPrefabData = BasicScenePrefab<(Vec<Position>, Vec<Normal>, Vec<TexCoord>)>;
 
-impl SimpleState for MyState {
-    fn on_start(&mut self, _data: StateData<'_, GameData<'_, '_>>) {}
+struct GameState;
+
+impl SimpleState for GameState {
+    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        let handle = data.world.exec(|loader: PrefabLoader<'_, MyPrefabData>| {
+            loader.load("sphere.ron", RonFormat, ())
+        });
+        data.world.create_entity().with(handle).build();
+    }
 }
 
 fn main() -> amethyst::Result<()> {
     amethyst::start_logger(Default::default());
 
     let app_root = application_root_dir()?;
-
     let config_dir = app_root.join("config");
+    let assets_dir = app_root.join("assets");
+
     let display_config_path = config_dir.join("display.ron");
 
+
     let game_data = GameDataBuilder::default()
+        .with(PrefabLoaderSystem::<MyPrefabData>::default(),"",&[])
+        .with_bundle(TransformBundle::new())?
         .with_bundle(
             RenderingBundle::<DefaultBackend>::new()
                 .with_plugin(
                     RenderToWindow::from_config_path(display_config_path)
-                        .with_clear([0.34, 0.36, 0.52, 1.0]),
+                        .with_clear([0.0, 0.0, 0.0, 1.0]),
                 )
-                .with_plugin(RenderFlat2D::default()),
+                .with_plugin(RenderShaded3D::default()),
         )?
-        .with_bundle(TransformBundle::new())?;
+        ;
 
-    let mut game = Application::new("/", MyState, game_data)?;
+    let mut game = Application::new(assets_dir, GameState, game_data)?;
     game.run();
 
     Ok(())
